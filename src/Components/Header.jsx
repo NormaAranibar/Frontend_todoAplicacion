24d1c3717954e231ef406e7deb542abb5f77be03
NormaAranibar <naranibarg@gmail.com>
2024-06-20 22:50:26 -0500

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Aplicacion de Tareas</h1>
+      <p>Estas son tareas que debo realizar</p>
+    </header>
+  );
+};
+
+export default Header;
