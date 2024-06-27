5911bca2011a029befd09171717828ee2c7c0e12
NormaAranibar <naranibarg@gmail.com>
2024-06-26 23:13:57 -0500

    estilos implementado en Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import styles from './Header.module.css'
 
 const Header = () => {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <p>Todo App</p>
-      <nav>
+      <nav className={styles.nav}>
         <Link to="/login">login</Link>
         <Link to="/">home</Link>
         <Link to="/about">about</Link>
